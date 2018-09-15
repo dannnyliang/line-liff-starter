3be5dbefb63c645b81e0250fd25cbdd6c5123dc2
danny <youchenliang@gmail.com>
2018-09-15 16:05:04 +0800

    liff init

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
     es6: true,
     node: true,
   },
+  globals: {
+    liff: true,
+  },
   rules: {
     "import/no-unresolved": "off",
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
--- src/App/index.js
+++ src/App/index.js
@@ -3,17 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let liffData;
+  window.liff.init(
+    (data) => {
+      liffData = data;
+      console.log(data);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
-      <p className="App-intro">
-        To get started, edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
+      <table>
+        <tbody>
+          <tr>
+            <th>language</th>
+            <td id="languagefield">{liffData.language}</td>
+          </tr>
+          <tr>
+            <th>context.viewType</th>
+            <td id="viewTypefield">{liffData.context.viewType}</td>
+          </tr>
+          <tr>
+            <th>context.userId</th>
+            <td id="userIdfield">{liffData.context.userId}</td>
+          </tr>
+          <tr>
+            <th>context.utouId</th>
+            <td id="utouIdfield">{liffData.context.utouId}</td>
+          </tr>
+          <tr>
+            <th>context.roomId</th>
+            <td id="roomIdfield">{liffData.context.roomId}</td>
+          </tr>
+          <tr>
+            <th>context.groupId</th>
+            <td id="groupIdfield">{liffData.context.groupId}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
